07af64e08168e7cddc1f710764b80935568d6931
MarcusS64 <marcus@skolds.com>
2022-11-21 10:20:09 +0100

    Added files

---
+++ Assets/Scripts/ClickOn.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOn : MonoBehaviour
+{
+    public Transform tokenObj_w;
+    public Transform tokenObj_b;
+    public Transform probeObj;
+    private int[,] directions = new int[,] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(GameFlow.currenTurn == "White")
+        {
+            Instantiate(tokenObj_w, transform.position, tokenObj_w.rotation);
+            GameFlow.currenTurn = "Black";
+            GetComponent<BoxCollider2D>().enabled = false;
+            Instantiate(probeObj, transform.position, probeObj.rotation);
+        }
+        else
+        {
+            Instantiate(tokenObj_b, transform.position, tokenObj_b.rotation);
+            GameFlow.currenTurn = "White";
+            GetComponent<BoxCollider2D>().enabled = false;
+            Instantiate(probeObj, transform.position, probeObj.rotation);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+    public Transform squareObj;
+    private float width;
+    private float height;
+    private int nrOfTiles = 4;
+    public static string currenTurn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = squareObj.GetComponent<SpriteRenderer>().bounds.size.x;
+        height = squareObj.GetComponent<SpriteRenderer>().bounds.size.y;
+        float posXstart = -width * (nrOfTiles / 2);
+        float posXend = width * (nrOfTiles / 2);
+        float posYstart = -height * (nrOfTiles / 2);
+        float posYend = height * (nrOfTiles / 2);
+        for (float x = posXstart; x < posXend; x += width) //float x = 0; x < width * nrOfTiles; x += width
+        {
+            //Instantiate(squareObj, new Vector2(x, 4), squareObj.rotation);
+            for (float y = posYstart; y < posYend; y += height)
+            {
+                Instantiate(squareObj, new Vector2(x, y), squareObj.rotation);
+            }
+        }
+        currenTurn = "White";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
