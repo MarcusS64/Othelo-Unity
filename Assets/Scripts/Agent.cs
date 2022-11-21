698af6eaa44a50e9435a1e744d03b0b688d5f2ef
fire-walk-with-me <mrc.e.bra@gmail.com>
2022-11-21 11:09:25 +0100

    updated agent

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -5,15 +5,65 @@ using UnityEngine;
 public class Agent : MonoBehaviour
 {
     double timer;
+    [SerializeField] double maxTime;
+    public bool active;
+    [SerializeField] bool alphaBetaPruning;
+
+    Node node;
+    Node bestPlacement;
+    Graph graph;
+    GameFlow gameFlow;
+
+    [SerializeField] Transform tokenObj_w;
+    [SerializeField] Transform tokenObj_b;
+    [SerializeField] Transform probeObj;
 
     void Start()
     {
-        
+        graph = GetComponent<Graph>();
+        gameFlow = GetComponent<GameFlow>();
+        timer = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (active)
+        {
+            timer += Time.deltaTime;
+
+            if (timer <= maxTime)
+            {
+                searchBestMove();
+            }
+            else
+            {
+                placeToken();
+                active= false;
+                timer = 0;
+            }
+        }
+    }
+
+    private void searchBestMove()
+    {
+
+    }
+
+    private void placeToken()
+    {
+        if (GameFlow.currenTurn == "White")
+        {
+            Instantiate(tokenObj_w, transform.position, tokenObj_w.rotation);
+            GameFlow.currenTurn = "Black";
+            GetComponent<BoxCollider2D>().enabled = false;
+            Instantiate(probeObj, transform.position, probeObj.rotation);
+        }
+        else
+        {
+            Instantiate(tokenObj_b, transform.position, tokenObj_b.rotation);
+            GameFlow.currenTurn = "White";
+            GetComponent<BoxCollider2D>().enabled = false;
+            Instantiate(probeObj, transform.position, probeObj.rotation);
+        }
     }
 }
--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -34,4 +34,6 @@ public class GameFlow : MonoBehaviour
     {
         
     }
+
+
 }
